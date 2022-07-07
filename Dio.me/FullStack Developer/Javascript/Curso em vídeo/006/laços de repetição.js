24ef8bfc5f0b6a7f1e6af16e308fb5bbdeb490ec
João <jpotakubr@gmail.com>
2022-07-07 14:19:01 -0300

    Modelo

---
+++ Dio.me/FullStack Developer/Javascript/Curso em vídeo/006/laços de repetição.js
@@ -0,0 +1,17 @@
+//teste lógico no inicio
+c=1
+while(c<=6){
+    console.log(`passo ${c} de c`);
+    c++
+}
+//teste lógico no final
+x=1
+do {
+    console.log(`passo ${x} de x`);
+    x++
+} while (x<=6)
+
+//laço de repetição com a estrutura for
+for( y=1; y<=6; y++){
+    console.log(`passo ${y} de y`);
+}
