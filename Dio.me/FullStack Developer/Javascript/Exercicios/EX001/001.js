aea5bfef5661fe1c74c5ec4b566051acecaed71e
João <jpotakubr@gmail.com>
2022-07-12 14:43:59 -0300

    update ex001

--- Dio.me/FullStack Developer/Javascript/Exercicios/EX001/001.js
+++ Dio.me/FullStack Developer/Javascript/Exercicios/EX001/001.js
@@ -1,5 +1,18 @@
-function myFunction(a, n){
+/*function myFunction(a, n){
     String(a)
     n= console.log(a[n-1]);
     return 
 }
+*/
+
+function nome(x) {
+   var num= String(x)
+   var num1 = num.slice(0,1)
+   var num2 = num.slice(1)
+   var sim1 = Number(num1)
+   var sim2 = Number(num2)
+   var soma = sim1 + sim2
+   console.log(soma);
+   console.log(num.slice(1));
+}
+nome(12)
