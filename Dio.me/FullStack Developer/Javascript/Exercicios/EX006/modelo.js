1802e9541dd2ef902773e6957324e05b496785c4
João <jpotakubr@gmail.com>
2022-07-12 21:51:48 -0300

    update

--- Dio.me/FullStack Developer/Javascript/Exercicios/EX006/modelo.js
+++ Dio.me/FullStack Developer/Javascript/Exercicios/EX006/modelo.js
@@ -1,13 +1,15 @@
-function contar() {
-        var inicio = document.getElementById('inicio')
-        var ini = Number(inicio.value)
-        var fim = document.getElementById('fim')
-        var fi = Number(fim.value)
-        var passo = document.getElementById('passo')
-        var pass = Number(passo.value)
-        var res = document.getElementById('res')
-        while(ini<=fim) {
-            ini+pass
-            res.innerHTML= `${ini}`
-     }
-}   
+function contar(){
+    var inicio = document.getElementById('inicio');
+    var fim = document.getElementById('fim');
+    var passo = document.getElementById("passo");
+    var res = document.getElementById("res")
+    var ini = String(inicio)
+    var fi = String(fim)
+    var pass = String(passo)
+
+    if (ini.length == 0 ){
+        window.alert(ini)
+    } else{
+        res.innerHTML = inicio
+    }
+}
