90f5eef64588c9a54af791bc6d47c777b04dbaff
João <jpotakubr@gmail.com>
2022-07-15 09:47:25 -0300

    EX007 finalizado

--- Dio.me/FullStack Developer/Javascript/Exercicios/EX007/modelo.js
+++ Dio.me/FullStack Developer/Javascript/Exercicios/EX007/modelo.js
@@ -1,3 +1,18 @@
 function criar() {
-    var aqui = window.docu
+    var numero = window.document.getElementById("aqui")
+    var tabuada = window.document.getElementById("tabuada")
+
+    if (numero.value.length == 0 ){
+        window.alert('Por favor, digite um número!')
+    } else{
+        var num = Number(numero.value)
+        c=1
+        tabuada.innerHTML= ''
+        while (c<11){
+            var item = window.document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c} `
+            tabuada.appendChild(item)
+            c++
+        }
+    }
 }
