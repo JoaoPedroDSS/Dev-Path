427a54ae8a5cd751bbb7eb012da7158bea43c78e
João <jpotakubr@gmail.com>
2022-07-18 08:48:39 -0300

    ex008

--- Dio.me/FullStack Developer/Javascript/Exercicios/EX008/modelo.js
+++ Dio.me/FullStack Developer/Javascript/Exercicios/EX008/modelo.js
@@ -1,10 +1,10 @@
-function analisa(){
+function analisa() {
     var numero = window.document.getElementById("aqui")
     var num = Number(numero.value)
-    var analisador = window.document.getElementById("analisador")
-
+    var analisador = window.document.getElementById("Analisador")
     var item = window.document.createElement('option')
-    item.text = `${num}`
+    item.text = `${num} e unas cossitas`
     analisador.appendChild(item)
-    
+    var listinha = [item]
+    window.alert(listinha)
 }
