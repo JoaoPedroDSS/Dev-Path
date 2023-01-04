80b9a08b437f8d8977f98d5e95bae5919f575691
João Pedro De Sousa Soares <90647844+JoaoPedroDSS@users.noreply.github.com>
2023-01-04 10:32:58 -0300

    utilizando coisas

--- Dio.me/FullStack Developer/React/my-app/src/App.js
+++ Dio.me/FullStack Developer/React/my-app/src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './HelloWorld';
 import SayMyName from './SayMyName';
 import Pessoa from './Pessoa';
 import Frase from './Frase';
+import List from './List';
 
 const name = 'Anthony'
 const url = "https://via.placeholder.com/150"
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
     <h1>Hail {name}</h1>
     <img src={url} alt="Minha foto"/>
+    <List/>
     <HelloWorld />
     <SayMyName name="Mateus" />
     <Pessoa nome="Carlo" idade="32"/>
---
+++ Dio.me/FullStack Developer/React/my-app/src/List.js
@@ -0,0 +1,13 @@
+import Item from "./Item"
+function List(){
+    return(
+        <>
+            <h1>Minha Lista</h1>
+            <ul>
+                <Item marca ="Forgiato"/>
+                <li>Item 2</li>
+            </ul>
+        </>
+    )
+}
+export default List
