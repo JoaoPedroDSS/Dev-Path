241b24e38e95aae2cd46278f223ae6043dc7070b
João Pedro De Sousa Soares <90647844+JoaoPedroDSS@users.noreply.github.com>
2023-01-06 09:42:54 -0300

    usando set state

--- Dio.me/FullStack Developer/React/my-app/src/forms.js
+++ Dio.me/FullStack Developer/React/my-app/src/forms.js
@@ -1,20 +1,37 @@
+import {useState} from 'react'
 function Formulário(){
     function cadastrarUsuario(e){
         e.preventDefault()
+        console.log(name);
         console.log('Cadastrou o usuário!');
-        
+        console.log(password);
     }
+    const[name, setName] = useState()
+    const[password, setPassword] = useState()
+
     return(
         <div>
             <h1>Cadastre o usuário</h1>
             <form onSubmit={cadastrarUsuario}>
             <div>
-                <input type="text" placeholder="Digite seu nome"></input>
+                <label htmlFor="name">Nome:</label>
+                <input 
+                type="text"
+                placeholder="Digite seu nome"
+                onChange={(e)=>setName(e.target.value)}></input>
+            </div>
+           
+            <div>
+                <label htmlFor="password">Senha:</label>
+                <input type="password"
+                  id="password"
+                  placeholder="Digite a sua senha"
+                  name="password"
+                  onChange={(e)=>setPassword(e.target.value)}></input>
             </div>
             <div>
                 <input type="submit" value="Cadastrar"></input>
             </div>
-            
             </form>
         </div>
     )
