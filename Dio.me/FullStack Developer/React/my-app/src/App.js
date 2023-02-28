31a373a1727e549b14740220d173ab9e44c5e69e
João Pedro De Sousa Soares <90647844+JoaoPedroDSS@users.noreply.github.com>
2023-02-28 20:20:35 -0300

    feito a parte de listas

--- Dio.me/FullStack Developer/React/my-app/src/App.js
+++ Dio.me/FullStack Developer/React/my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import OutraLista from './Outralista';
 function App() {
   const meusItens= ['React', 'Vue','Angular']
   return (
     <div className="App">
      <h1>Renderização de Listas</h1>
-     
+     <OutraLista itens={meusItens}  />
+     <OutraLista itens={''}  />
     </div>
     
   );
