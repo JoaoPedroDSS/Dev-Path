90a6f7fa8ef1b30eb3f25951211773881cb02496
João Pedro De Sousa Soares <90647844+JoaoPedroDSS@users.noreply.github.com>
2023-03-13 15:48:52 -0300

    vou apagar tudo mesmo então fds

--- Dio.me/FullStack Developer/React/my-app/src/App.js
+++ Dio.me/FullStack Developer/React/my-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes,Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes,Route, Link} from "react-router-dom";
 import Home from './pages/Home'
 import Empresa from './pages/Empresa'
 import Contato from './pages/Contato'
@@ -8,21 +8,22 @@ function App() {
   return (
     <Router>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/empresa">Empresa</Link></li>
-        <li><Link to="/contato">Contato</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/empresa">Empresa</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
       </ul>
-      <Routes>
-        <Route exact path="/">
+
+     <Routes>
+        <Route path="/">
           <Home/>
         </Route>
-        <Route path="/contato">
-          <Contato/>
-        </Route>
-        <Route path="/empresa">
-          <Empresa/>
-        </Route>
-      </Routes>
+     </Routes>
     </Router>
   );
 
